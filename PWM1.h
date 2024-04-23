f7c87ea1d081fb16afb1f9cba1e2915daa539189
JPSan44 <62415179+joaosantos44@users.noreply.github.com>
2024-04-23 13:47:51 -0600

    PWM1.h

---
+++ PWM1.h
@@ -0,0 +1,26 @@
+/*
+ * PWM1.h
+ * Created: 22/04/2024 17:31:08
+ *  Author: joaos
+ */ 
+
+
+#ifndef PWM1_H_
+#define PWM1_H_
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#define Nor 0
+#define Inv 1
+
+#define bits_8 0
+#define bits_9 1
+#define bits_10 2
+#define ICR1_set 3
+#define OCR1_set 4
+
+void setPWM1A(uint8_t INV_NOR, uint8_t ModoOnda, uint16_t Prescaler);
+void DutyA1(uint8_t DutyCycle);
+
+#endif /* PWM1_H_ */
